7ec51527f8e26d2874e6bc47c6ba5aeb84ed12d8
Vivek_Kote <133883119+vivek-9850@users.noreply.github.com>
2024-01-09 23:23:40 +0530

    Create Buffer.java

---
+++ Buffer.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Buffer 
+{
+    public static void main(String arg[]) 
+    {
+        try
+        {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("Enter first number : ");
+            int no1 = Integer.parseInt(br.readLine());
+
+            System.out.println("Enter second number : ");
+            int no2 = Integer.parseInt(br.readLine());
+
+            int Ans = no1 + no2;
+            System.out.println("Addition is "+Ans);
+        }
+        catch(Exception obj)
+        {}
+    }    
+}
