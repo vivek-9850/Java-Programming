d4ae7c19d078164d3d481b3ab44e6aae18c94f64
Vivek_Kote <133883119+vivek-9850@users.noreply.github.com>
2024-01-09 23:52:30 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,14 @@
+
+class Demo
+{
+    public static void main(String arg[])
+    {
+        System.out.println("Jay Ganesh...");
+    }
+}
+
+//compiled the code
+//javc Demo.java
+
+//Execute the code
+//jave Demo
