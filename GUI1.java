0519408bd0b9b5273389371f02910b1701a4b42d
Vivek_Kote <133883119+vivek-9850@users.noreply.github.com>
2024-01-13 19:32:18 +0530

    Create GUI1.java

---
+++ GUI1.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+class GUI1 
+{
+    public static void main(String arg[])
+    {
+        Frame fobj = new Frame("Marvellous");
+        fobj.setSize(600,600);
+        fobj.setVisible(true);
+    }    
+}
