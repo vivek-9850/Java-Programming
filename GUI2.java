19fce87bc094f6b0af56b8f226834dabc83fc955
Vivek_Kote <133883119+vivek-9850@users.noreply.github.com>
2024-01-13 19:33:04 +0530

    Create GUI2.java

---
+++ GUI2.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+
+class MarvellousFrame
+{
+    public MarvellousFrame(String title)
+    {
+        Frame fobj = new Frame();
+        fobj.setSize(300,600);  //(W,H)
+        fobj.setVisible(true);
+    }
+}
+
+class GUI2 
+{
+    public static void main(String arg[])
+    {
+        MarvellousFrame mobj = new MarvellousFrame("PPA49");
+        
+    }        
+}
