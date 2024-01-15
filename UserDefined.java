06494fbdc9e927e3df707e5f8e10369aa8d7e541
Vivek_Kote <133883119+vivek-9850@users.noreply.github.com>
2024-01-15 22:42:21 +0530

    Create UserDefined.java

---
+++ UserDefined.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class AgeInvalid extends Exception
+{
+    public AgeInvalid(String str)
+    {
+        super(str);
+    }
+} 
+
+class UserDefined
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        System.out.println("Enter your age : ");
+        int iAge = sobj.nextInt();
+
+        try
+        {
+            if(iAge < 18)
+            {
+                AgeInvalid aobj = new AgeInvalid("Your age is below 18");
+                throw aobj;
+                // throw new AgeInvalid("your age is below 18");
+            }
+            else
+            {
+                System.out.println("Login Succesful..");
+            }
+        }
+        catch(AgeInvalid obj)
+        {
+            System.out.println("Inside catch block");
+            System.out.println(obj);
+        }
+    }
+}
