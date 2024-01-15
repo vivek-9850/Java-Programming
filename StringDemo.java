98326c5786c8febd46556c254431cfc00cf3cc17
Vivek_Kote <133883119+vivek-9850@users.noreply.github.com>
2024-01-15 22:38:13 +0530

    Create StringDemo.java

---
+++ StringDemo.java
@@ -0,0 +1,16 @@
+class StringDemo 
+{
+    public static void main(String arg[])
+    {
+        String str1 = "Hello";
+        String str2 = new String("Hello");
+        String str3 = "Hello";
+        String str4 = "Marvellous";
+        String str5 = new String("Hello");
+        String str6 = new String("world");
+        String str7 = "world";
+        String str8 = "world";
+
+        System.out.println("Length os str1 is : "+str1.length());
+    } 
+}
